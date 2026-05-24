cfcaf694f8a059affc5cae3b3d128ceda9dad483
Adrien <adrienvalerio2022@gmail.com>
2026-05-24 14:30:36 -0300

    alteraçao

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>© 2025 Loja Next — Projeto Acadêmico</p>
+      <p>© 2025 Loja do Baião — Projeto Acadêmico</p>
     </footer>
   )
 }
